481e724e83676de2f9795bb9cf48e2e955b7751c
Jens Joy <jensjoy54@gmail.com>
2025-06-28 23:27:22 +0530

    ec2

---
+++ ec2/main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  key_name      = "app-ssh-key"
+tags = {
+    Name = var.ec2_name  
+  }
+}
+ 
