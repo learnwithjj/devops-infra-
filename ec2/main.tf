39869a51868abaf9bee996a83a4dea3550b99a88
Apple <apple@Sussegado-MacBook-Air.local>
2025-07-03 11:54:11 +0530

    changes

--- ec2/main.tf
+++ ec2/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "app_server" {
   instance_type = "t3.micro"
   key_name      = "app-ssh-key"
 tags = {
-    Name = var.ec2_name  
+    Name = "${var.ec2_name}-${var.environment}"  
   }
 }
  
