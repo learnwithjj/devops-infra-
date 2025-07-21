d0105a97fe5045e3d83d1206a25dca3e6b64f881
Apple <apple@Sussegado-MacBook-Air.local>
2025-07-21 22:48:29 +0530

    removing zone

--- rds/modules/rds/main.tf
+++ rds/modules/rds/main.tf
@@ -29,7 +29,6 @@ resource "aws_db_subnet_group" "default" {
 resource "aws_subnet" "subnet1" {
   vpc_id     = var.vpc_id
   cidr_block = "172.31.0.0/24"
-  availability_zone = var.zones
   tags = {
     Name = "subnet-rds-${var.environment}-1"
   }
